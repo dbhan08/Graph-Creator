a78a57b5f7d437a8f091d91ca81a2a6aa5ba7fcd
dbhan08 <38201130+dbhan08@users.noreply.github.com>
2020-05-20 20:18:28 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -28,8 +28,50 @@ void addVertex(vector<node*> nodes, char* label) {
 
 
 
-void addEdge(vector<node*> nodes, char* label) {
+void addEdge(vector<node*> nodes,vector<node*> edges,char* firstLabel, char* secondLabel ,char* label) {
+    edge* temp = new edge(label);
+    node* first = NULL;
+    node* second = NULL;
+    bool there = false;
     
+    vector<node*>:: iterator i;
+    for(i= nodes.begin(); i != nodes.end(); i++) {
+        
+        
+        if(*(*i->getLabel())==*firstLabel) {
+            first = *i;
+            
+        } else if(*(*i->getLabel())==*secondLabel) {
+            second = *i;
+        }
+        
+        
+    }
+    
+    if(first != second) {
+        vector<edge*>::iterator j;
+        for(j=edges.begin(); j != edges.end(); j++) {
+            if(*j->getFirst == first && *j->getSecond() == second) {
+                there = true;
+            }
+        }
+        
+        if(there) {
+            cout << "The edge already exists";
+        } else if(first != NULL && second != NULL) {
+            temp->setFist(first);
+            temp->setSecond(second);
+            edges.push_back(temp);
+            
+        } else {
+            cout << "Some node or nodes don't exist" << endl;
+        }
+        
+        
+    } else {
+        cout <<  "Can't have edge between the same nodes" << endl;
+        return;
+    }
     
     
 }
